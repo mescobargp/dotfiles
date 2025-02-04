9eabc078c552d16d9f0e5f5352c7cf34d0906393
mescobargp <mescobar@panoramaed.com>
2025-02-04 11:49:46 -0500

    Add changes to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Make sure the script is executable
 chmod u+x bootstrap.sh
 
 # Copy the .zshrc file to the home directory
+CODESPACES_HOME="/workspaces/.codespaces/.persistedshare/dotfiles"
 cp "${CODESPACES_HOME}/.zshrc" "${HOME}/.zshrc"
